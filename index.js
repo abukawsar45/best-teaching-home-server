8bdfd2e2a4ced2237d227eb07362139dbe10ab79
abukawsar45 <kawsar1146236@gmail.com>
2023-06-07 08:01:19 +0600

    connected mongoDb

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// RkIXBzpysIQwLUAl
+
+
+const uri = "mongodb+srv://bestSir:RkIXBzpysIQwLUAl@cluster0.9lqzgjv.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
   res.send('best teaching home server is running!');
 });
-
-
-
+app.get('/school', (req, res) => {
+  res.send('best teaching school server is running!');
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
